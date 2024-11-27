347a2e3562231e6efe9b3575c7625ed3261f3f90
marko1684 <marko@math.com>
2024-11-27 21:30:24 +0100

    Integrating Lexer and Parser into Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
-package org.example;
+import java.io.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        if (args.length != 1) {
+            System.err.println("Usage: java Main <file>");
+            System.exit(1);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String filePath = args[0];
+
+        try {
+            Reader reader = new FileReader(filePath);
+
+            Lexer lexer = new Lexer(reader);
+            parser p = new parser(lexer);
+
+            p.parse();
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
